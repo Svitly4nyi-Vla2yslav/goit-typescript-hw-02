15595427cc8208f0835741766469ef3551394976
Svitly4nyi-Vla2yslav <126347711+Svitly4nyi-Vla2yslav@users.noreply.github.com>
2023-10-26 23:53:19 +0200

    generic task-3

--- src/generic/2.ts
+++ src/generic/2.ts
@@ -11,7 +11,10 @@ type AllType = {
   weight: number
 }
 
-function compare (top, bottom): AllType {
+type Top = Pick<AllType, 'name' | 'color'>;
+type Bottom = Pick<AllType, 'position' | 'weight'>
+
+function compare (top: Top , bottom: Bottom): AllType {
   return {
     name: top.name,
     color: top.color,
@@ -19,5 +22,6 @@ function compare (top, bottom): AllType {
     weight: bottom.weight,
   }
 }
+
 
 export {};
